3949ca88ef1aef92a5c5f72dc41bf39c192571b0
jljl1337 <lckjack123@gmail.com>
2025-06-29 17:42:34 -0600

    feat: update main function

--- cmd/blackjack-simulator/main.go
+++ cmd/blackjack-simulator/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-
-	"github.com/jljl1337/blackjack-simulator/internal/core"
+	"github.com/jljl1337/blackjack-simulator/internal/simulation"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-
-	shoe := core.NewShoe(8, 0.75, rand.New(rand.NewSource(1337)))
-	fmt.Println("First card in shoe:", shoe.Deal())
+	s := simulation.NewSimulator()
+	s.Run()
 }
