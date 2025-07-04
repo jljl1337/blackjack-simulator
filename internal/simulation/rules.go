d5eb15d86d81f71ed489d3c70d2a2c205ca5386d
jljl1337 <lckjack123@gmail.com>
2025-07-04 02:49:51 -0600

    feat: add more rules in simulator configuration

--- internal/simulation/rules.go
+++ internal/simulation/rules.go
@@ -5,20 +5,42 @@ import (
 )
 
 // Rules implements the rules of the game for a single round of blackjack.
-type Rules struct{}
+type Rules struct {
+	doubleAfterSplit    bool
+	hitAfterSplitAce    bool
+	splitAfterSplitAce  bool
+	doubleAfterSplitAce bool
+	maxNumHands         int
+}
 
 // NewRules creates a new instance of PlayRules.
-func NewRules() Rules {
-	return Rules{}
+func NewRules(doubleAfterSplit, hitAfterSplitAce, splitAfterSplitAce, doubleAfterSplitAce bool, maxNumHands int) Rules {
+	return Rules{
+		doubleAfterSplit:    doubleAfterSplit,
+		hitAfterSplitAce:    hitAfterSplitAce,
+		splitAfterSplitAce:  splitAfterSplitAce,
+		doubleAfterSplitAce: doubleAfterSplitAce,
+		maxNumHands:         maxNumHands,
+	}
 }
 
 func (r Rules) GetActionsAllowed(currentHandSize int, numHands int, splitAce bool) (map[blackjack.Action]bool, error) {
 	// This method should return the actions available to the player.
+
+	// Check if hit is allowed for split aces
+	canHit := !splitAce || r.hitAfterSplitAce
+
+	// Check if double is allowed
+	canDouble := currentHandSize == 2 && (!splitAce || r.doubleAfterSplitAce) && (numHands < 2 || r.doubleAfterSplit)
+
+	// Check if split is allowed
+	canSplit := ((!splitAce || r.splitAfterSplitAce) && numHands < r.maxNumHands) || r.maxNumHands < 0
+
 	return map[blackjack.Action]bool{
-		blackjack.Hit:       !splitAce,
+		blackjack.Hit:       canHit,
 		blackjack.Stand:     true,
-		blackjack.Double:    !splitAce && numHands < 2 && currentHandSize == 2,
-		blackjack.Split:     !splitAce && numHands < 4,
+		blackjack.Double:    canDouble,
+		blackjack.Split:     canSplit,
 		blackjack.Surrender: true,
 	}, nil
 }
